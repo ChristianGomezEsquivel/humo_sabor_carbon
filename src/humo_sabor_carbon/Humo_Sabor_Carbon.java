4039e4d6a1ed5212cd9d3109229ff2b2a3421e4b
ChristianGomezEsquivel <christiangomez1@outlook.es>
2023-11-22 15:25:20 -0600

    Se agrego la clase principal de todo el proyecto

---
+++ src/humo_sabor_carbon/Humo_Sabor_Carbon.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package humo_sabor_carbon;
+
+import Vista.*;
+
+/**
+ *
+ * @author chris
+ */
+public class Humo_Sabor_Carbon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login v1 = new Login(); // Creacion del objeto para llamar la ventana de inicio
+        v1.setVisible(true);
+    }
+    
+}
